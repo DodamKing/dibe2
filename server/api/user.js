8b3625c457402549af896db5e74e10790adde943
dodamking <ddkim@scsai.net>
2024-09-13 11:12:31 +0900

    세션 save도 없애버림

--- server/api/user.js
+++ server/api/user.js
@@ -46,17 +46,17 @@ router.post('/login', isNotAuthenticated, async (req, res) => {
         req.session.user = sessionUser
         
         // 세션 저장 완료 후 응답
-        req.session.save((err) => {
-            if (err) {
-                console.error('세션 저장 오류', err);
-                return res.status(500).json({ message: '세션 저장 중 오류가 발생했습니다.', code: 5 });
-            }
-            console.log('로그인 할 때 세션 아이디:', req.sessionID)
+        // req.session.save((err) => {
+        //     if (err) {
+        //         console.error('세션 저장 오류', err);
+        //         return res.status(500).json({ message: '세션 저장 중 오류가 발생했습니다.', code: 5 });
+        //     }
             
-            res.json({ message: '로그인 성공', user: sessionUser, code: 1 });
-        });
-
-        // res.json({ message: '로그인 성공', user: { userId: user._id, username: user.username, email: user.email }, code: 1})
+        //     res.json({ message: '로그인 성공', user: sessionUser, code: 1 });
+        // });
+        
+        console.log('로그인 할 때 세션 아이디:', req.sessionID)
+        res.json({ message: '로그인 성공', user: { userId: user._id, username: user.username, email: user.email }, code: 1})
     } catch (err) {
         console.error('로그인 에러', err)
         res.status(500).json({ message: '서버 오류가 발생했습니다.', code: 4})
