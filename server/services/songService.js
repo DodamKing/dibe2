5b1de22e879edf85417d51333575db61e4c6c2fc
Dodamking <power6120@naver.com>
2024-09-13 22:17:22 +0900

    헤더에 더 많은 옵션 추가

--- server/services/songService.js
+++ server/services/songService.js
@@ -191,8 +191,13 @@ module.exports = {
             requestOptions: {
                 headers: {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                    'Accept-Language': 'en-US,en;q=0.9',
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                    'Connection': 'keep-alive',
+                    'Referer': 'https://www.youtube.com/',
                 },
             },
+            highWaterMark: 1 << 25,
         }
 
         try {
