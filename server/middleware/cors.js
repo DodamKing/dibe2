e0c754cbf712426c0699dfbbd6b33d1023c6162a
dodamking <ddkim@scsai.net>
2024-09-24 17:48:34 +0900

    cors 설정 수정함

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -2,21 +2,20 @@ export default function (req, res, next) {
     const allowedOrigins = [
         'https://www.youtube.com',
         'https://www.youtube-nocookie.com',
-        process.env.NODE_ENV === 'production'
-            ? 'https://dibe2.dimad.site'
-            : 'http://localhost:3000'
+        'https://dibe2.dimad.site',
+        'http://localhost:3000'
     ];
-    const origin = req.headers.origin;
 
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
+    const origin = req.headers.origin;
 
     if (allowedOrigins.includes(origin)) {
         res.setHeader('Access-Control-Allow-Origin', origin);
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         res.setHeader('Access-Control-Allow-Credentials', 'true');
+    } else {
+        // 허용되지 않은 origin인 경우, 기본 origin 설정
+        res.setHeader('Access-Control-Allow-Origin', process.env.NODE_ENV === 'production' ? 'https://dibe2.dimad.site' : 'http://localhost:3000');
     }
 
     // preflight request 처리
@@ -26,5 +25,5 @@ export default function (req, res, next) {
         return;
     }
 
-    next()
+    next();
 }
