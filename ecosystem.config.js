9ede59ae31021ad38e11e8c4842de6212f946d4d
Dodamking <power6120@naver.com>
2024-10-12 16:54:41 +0900

    log date format 해야 시간이 나오긴 하는듯

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
         // env: {
         //     TZ: 'Asia/Seoul'
         // },
-        // log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+        log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
         output: './logs/out.log',
         error: './logs/error.log',
         max_memory_restart: '1G'  // 메모리 사용량이 1GB를 초과하면 재시작
