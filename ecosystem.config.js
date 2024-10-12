07951e585d8aa6e8a7ab912f20630bdde122e16a
Dodamking <power6120@naver.com>
2024-10-12 16:44:47 +0900

    pm2 설정파일 바꿔봄

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,16 +3,12 @@ module.exports = {
         name: 'dibe2',
         script: 'npm',
         args: 'start',
-        env: {
-            TZ: 'Asia/Seoul'
-        },
-        log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-        out_file: './logs/out.log',
-        error_file: './logs/error.log',
-        log_file: './logs/combined.log',
-        // restart_delay: 3000,  // 3초 대기 후 재시작
-        // max_restarts: 10,     // 최대 10번 재시작 시도
-        // cron_restart: '0 4 * * *',  // 4시 재시작
+        // env: {
+        //     TZ: 'Asia/Seoul'
+        // },
+        // log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+        output: './logs/out.log',
+        error: './logs/error.log',
         max_memory_restart: '1G'  // 메모리 사용량이 1GB를 초과하면 재시작
     }]
 };
