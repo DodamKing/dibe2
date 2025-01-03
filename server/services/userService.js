84c212445b75624853405cab025f499021e7bef4
DodamKing <power6120@naver.com>
2025-01-03 16:27:12 +0900

    카카오 로그인 리다이렉션 uri 변경

--- server/services/userService.js
+++ server/services/userService.js
@@ -8,7 +8,8 @@ const GOOGLE_REDIRECT_URI = NODE_ENV === 'production'
     : 'http://localhost:3000/api/users/google/callback'
 
     const KAKAO_REDIRECT_URI = NODE_ENV === 'production'
-    ? 'https://dibe2.dimad.site/api/users/kakao/callback'
+    // ? 'https://dibe2.dimad.site/api/users/kakao/callback'
+    ? 'http:///3.39.122.169/api/users/kakao/callback'
     : 'http://localhost:3000/api/users/kakao/callback'
 
 class UserService {
