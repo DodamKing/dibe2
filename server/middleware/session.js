0536b4ba21111e34611dadc5ebdb373e3defc897
DodamKing <power6120@naver.com>
2025-01-03 16:47:55 +0900

    samesite도 lax로 수정해봄

--- server/middleware/session.js
+++ server/middleware/session.js
@@ -15,10 +15,11 @@ const sessionMiddleware = session({
     }),
     cookie: {
         // secure: process.env.NODE_ENV === 'production',
-        secure: false, // http 사용시 반드시 false, true로 하면 쿠키 전송이 안됨됨
+        secure: false, // http 사용시 반드시 false, true로 하면 쿠키 전송이 안됨
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000,
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
+        // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
+        sameSite: 'lax' // 이것도 lax로 고정해보자자
     },
     name: 'dibe2_session_cookie'
 })
